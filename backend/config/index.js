cdd5a95eab5e29e33dc01b6a1933391e594be960
Kareem Hunte <kareemhunte@gmail.com>
2023-10-01 15:24:52 -0400

    frontend login

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,23 +1,25 @@
+require('dotenv').config()
+
 let config = {}
 
 // common
 config.nodeEnv = process.env.NODE_ENV || 'development'
 config.port = process.env.PORT || 8080
-config.baseUrl = `http://127.0.0.1:${config.port}`
+config.baseUrl = `http://localhost:${config.port}`
 config.logLevel = process.env.LOG_LEVEL || 'debug'
 
 // cross-origin resource sharing
 config.cors = {}
-config.cors.origin = process.env.CORS_ORIGIN || ['http://127.0.0.1:8080']
+config.cors.origin = process.env.CORS_ORIGIN || ['http://localhost:8082']
 
 // strategy
 config.auth = {}
 config.auth.google = {}
-config.auth.google.clientId = process.env.AUTH_GOOGLE_CLIENT_ID || 'sdf'
-config.auth.google.clientSecret = process.env.AUTH_GOOGLE_CLIENT_SECRET || 'sdfs'
+config.auth.google.clientId = process.env.AUTH_GOOGLE_CLIENT_ID || 'change-me'
+config.auth.google.clientSecret = process.env.AUTH_GOOGLE_CLIENT_SECRET || 'change-me'
 config.auth.facebook = {}
-config.auth.facebook.clientId = process.env.AUTH_FACEBOOK_CLIENT_ID || 'sdfs'
-config.auth.facebook.clientSecret = process.env.AUTH_FACEBOOK_CLIENT_SECRET || 'sfsdnpm'
+config.auth.facebook.clientId = process.env.AUTH_FACEBOOK_CLIENT_ID || 'change-me'
+config.auth.facebook.clientSecret = process.env.AUTH_FACEBOOK_CLIENT_SECRET || 'change-me'
 
 // session
 config.session = {}
